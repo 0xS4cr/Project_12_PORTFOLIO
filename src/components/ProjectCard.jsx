572207654c86e541daf8e3e551cff435d24ac2ca
Clément RENAUD <122524370+0xS4cr@users.noreply.github.com>
2023-11-27 22:09:01 +0100

    Add files via upload

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,19 +2,35 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
+import booki from "../assets/projectsImg/booki.png";
+import doticon from "../assets/projectsImg/doticon.png";
+import morpion from "../assets/projectsImg/morpion.png";
+import ohmyfood from "../assets/projectsImg/ohmyfood.png";
+import portfolio1 from "../assets/projectsImg/portfolio1.png";
+import sheeplabs from "../assets/projectsImg/sheeplabs.png";
+
+const images = {
+    booki,
+    doticon,
+    morpion,
+    ohmyfood,
+    portfolio1,
+    sheeplabs
+  };
+
 const ProjectCard = ({ projet }) => {
     const handleCardClick = () => {
         window.open(projet.icones.siteWeb, '_blank');
     };
 
     const handleCodeClick = (e) => {
-        e.stopPropagation(); // Empêche la propagation à l'élément parent
+        e.stopPropagation(); 
         window.open(projet.icones.gitHub, '_blank');
     };
 
     return (
         <div className="project-card" onClick={handleCardClick}>
-            <img src={projet.image} alt={projet.nom} className="project-image"/>
+            <img src={images[projet.image]} alt={projet.nom} className="project-image"/>
             <div className="project-content">
                 <h3 className="project-title">{projet.nom}</h3>
                 <p className="project-description">{projet.description}</p>
